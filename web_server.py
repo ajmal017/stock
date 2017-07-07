ea6d0849a1103bdb876434eceff0341cdd75ac53
liulaoye <liu_kun@msn.com>
2017-07-07 18:09:08 +0800

    kdj

--- web_server.py
+++ web_server.py
@@ -1,6 +1,7 @@
 import tornado.ioloop
 import tornado.web
 import tushare as ts
+import numpy as np
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -28,6 +29,29 @@ def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
     ])
+
+
+def KDJ(date, n=9, m1=3, m2=3):
+    datelen = len(date)
+    array = np.array(date)
+    kdjarr = []
+    for i in range(datelen):
+        if i - n < 0:
+            b = 0
+        else:
+            b = i - n + 1
+        rsvarr = array[b:i + 1, 0:5]
+        rsv = (float(rsvarr[-1, -1]) - float(min(rsvarr[:, 3]))) / (
+            float(max(rsvarr[:, 2])) - float(min(rsvarr[:, 3]))) * 100
+        if i == 0:
+            k = rsv
+            d = rsv
+        else:
+            k = 1 / float(m1) * rsv + (float(m1) - 1) / m1 * float(kdjarr[-1][2])
+            d = 1 / float(m2) * k + (float(m2) - 1) / m2 * float(kdjarr[-1][3])
+        j = 3 * k - 2 * d
+        kdjarr.append(list((rsvarr[-1, 0], rsv, k, d, j)))
+    return kdjarr
 
 
 if __name__ == "__main__":
