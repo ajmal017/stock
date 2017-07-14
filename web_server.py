61c2954d51d50c8fb963edc67ce553068c26dc08
liulaoye <liu_kun@msn.com>
2017-07-14 15:14:55 +0800

    获取当前股价

--- web_server.py
+++ web_server.py
@@ -1,19 +1,61 @@
+# -*- coding: utf-8 -*-
 import tornado.ioloop
 import tornado.web
 import tushare as ts
-import numpy as np
+
+
+# import numpy as np
+# from wxpy import *
+# from wechat_sender import *
+
+class GetCurrentPrice(tornado.web.RequestHandler):
+    def data_received(self, chunk):
+        pass
+
+    def get(self, stock_id):
+        # df = ts.get_today_all()
+        # print(df)
+        # df = df.ix[df.code == stock_id]
+        df = ts.get_realtime_quotes(stock_id)
+        print(df)
+        self.write(df.to_json(orient='records', force_ascii=False))
 
 
 class MainHandler(tornado.web.RequestHandler):
     def data_received(self, chunk):
         pass
 
+    """
+    获取k线数据
+      注意：
+      如果指定了start或者end，即使end日期远大于当前日起，返回的数据不含当天的k线数据，这个需要修正一下
+      
+    return
+    -------
+      DataFrame
+          date 交易日期 (index)
+          open 开盘价
+          high  最高价
+          close 收盘价
+          low 最低价
+          volume 成交量
+          amount 成交额
+          turnoverratio 换手率
+          code 股票代码
+    """
+
     def get(self):
         try:
             code = self.get_argument('code')
             ktype = self.get_argument('ktype', 'D')
             count = self.get_argument('count', '100')
-            df = ts.get_k_data(code, ktype=ktype).tail(int(count))
+            start = self.get_argument('start', '')
+            end = self.get_argument('end', '')
+            if start == '' or end == '':
+                df = ts.get_k_data(code, ktype=ktype, autype='qfq').tail(int(count))
+            else:
+                df = ts.get_k_data(code, ktype=ktype, autype='qfq', start=start, end=end)
+                # df1 = ts.get_k_data(code, ktype=ktype, autype='qfq').tail(int(count))
             print(df)
             # df = df[['date', 'open', 'close']]
             self.write(df.to_json(orient='records', force_ascii=False))
@@ -21,37 +63,12 @@ class MainHandler(tornado.web.RequestHandler):
         except tornado.web.MissingArgumentError:
             self.write('必须提供code参数')
 
-            # df = ts.get_hist_data(code)
-            # self.write(df.to_json(orient='records', force_ascii=False))
-
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
+        (r'/getCurrentPrice/(.*)', GetCurrentPrice),
     ])
-
-
-def KDJ(date, n=9, m1=3, m2=3):
-    datelen = len(date)
-    array = np.array(date)
-    kdjarr = []
-    for i in range(datelen):
-        if i - n < 0:
-            b = 0
-        else:
-            b = i - n + 1
-        rsvarr = array[b:i + 1, 0:5]
-        rsv = (float(rsvarr[-1, -1]) - float(min(rsvarr[:, 3]))) / (
-            float(max(rsvarr[:, 2])) - float(min(rsvarr[:, 3]))) * 100
-        if i == 0:
-            k = rsv
-            d = rsv
-        else:
-            k = 1 / float(m1) * rsv + (float(m1) - 1) / m1 * float(kdjarr[-1][2])
-            d = 1 / float(m2) * k + (float(m2) - 1) / m2 * float(kdjarr[-1][3])
-        j = 3 * k - 2 * d
-        kdjarr.append(list((rsvarr[-1, 0], rsv, k, d, j)))
-    return kdjarr
 
 
 if __name__ == "__main__":
